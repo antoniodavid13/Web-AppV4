c619a263c244002d1e6f1e8bca5730c7ed98c03c
antoniodavid13 <antoniodavidfernandezdegomar@gmail.com>
2025-05-15 13:54:16 +0200

    Prueba

---
+++ public/src/componentes/modal.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom'; 
+ // Asegúrate de tener el archivo de estilos correspondiente
+
+const VeterinarianModal = ({ show, message, onClose }) => {
+    const navigate = useNavigate();  // Inicializamos el hook useNavigate
+
+  // Si el modal no debe mostrarse, no renderiza nada
+  if (!show) return null;
+
+  const handleClose = () => {
+    // Si hay una redirección definida, redirige a esa ruta
+    navigate("/login");  // Si no hay redirección, simplemente cierra el modal
+    };
+
+// Si no debe mostrarse, no renderiza nada
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-header justify-content-center">
+          <h3>Mundo Camilo</h3>
+        </div>
+        <div className="modal-body">
+          <p>{message}</p>
+        </div>
+        <div className="modal-footer  justify-content-center">
+          <button className="close-modal-button" onClick={handleClose}>Cerrar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VeterinarianModal;
+
